48803b2f6ad502d5ead23328620e987be9da60cc
Zombie-01 <mm6816557@gmail.com>
2022-10-24 20:53:13 +0800

    asd

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import Layout from "../containers/layout";
 import "../css/global.css";
 import "../css/bootstrap.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "../css/font-awesome.min.css";
 import Script from "next/script";
 import "../css/responsive.css";
@@ -11,6 +11,7 @@ import Link from "next/link";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <></>
       <link
         href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,500,700"
         rel="stylesheet"
